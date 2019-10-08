464aa962cfe5a68e97bb6030b6e64afdc18e0ced
Szymon Witamborski <simon@brainshave.com>
2019-10-08 23:00:18 +0900

    Init

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const i3 = require('i3').createClient({ path: process.env.I3SOCK });
+
+const watchedWindowEvens = new Set(['focus']);
+
+let prev = null;
+
+i3.on('window', (event) => {
+  if(watchedWindowEvens.has(event.change)) {
+    i3.command(`[con_id=${prev}] opacity 0.95`);
+    i3.command('opacity 1');
+    prev = event.container.id;
+  }
+});
