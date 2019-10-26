eb2d9e52a0e8987414226519c8982cca8bb94544
Szymon Witamborski <simon@brainshave.com>
2019-10-26 16:50:25 +0900

    Apply opacity on start

--- index.js
+++ index.js
@@ -2,14 +2,39 @@
 
 const i3 = require('i3').createClient({ path: process.env.I3SOCK });
 
-const watchedWindowEvens = new Set(['focus']);
+const walk = (nodes, cb) => {
+  for (const node of nodes) {
+    cb(node);
+    if (node.nodes) {
+      walk(node.nodes, cb);
+    }
+  }
+}
 
+const setOpacity = (id, opacity) => i3.command(`[con_id=${id}] opacity ${opacity}`);
+const up = (id) => setOpacity(id, 1);
+const down = (id) => setOpacity(id, 0.9);
+
+const watchedWindowEvents = new Set(['focus']);
 let prev = null;
 
-i3.on('window', (event) => {
-  if(watchedWindowEvens.has(event.change)) {
-    i3.command(`[con_id=${prev}] opacity 0.90`);
-    i3.command('opacity 1');
-    prev = event.container.id;
+i3.tree((err, root) => {
+  walk([root], ({ type, id, focused }) => {
+    if (type === 'con') {
+      if (focused) {
+        prev = id;
+        up(id);
+      } else {
+        down(id);
+      }
+    }
+  });
+});
+
+i3.on('window', ({ change, container: { id } }) => {
+  if(watchedWindowEvents.has(change)) {
+    down(prev);
+    up(id);
+    prev = id;
   }
 });
